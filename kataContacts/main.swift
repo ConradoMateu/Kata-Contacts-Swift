d7506fa46e5b916199eb45bf9f30190556deb8ea
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2016-04-23 13:52:08 +0200

    Initial Commit

---
+++ kataContacts/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  kataContacts
+//
+//  Created by Conrado Mateu Gisbert on 23/04/16.
+//  Copyright © 2016 conradomateu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
